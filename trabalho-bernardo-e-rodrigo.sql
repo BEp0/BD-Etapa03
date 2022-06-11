2807a439d160bc91b8f9409f373de1b76f7832fe
Bernardo Porto e Silva Souza <72446326+BEp0@users.noreply.github.com>
2022-06-11 17:28:13 -0300

    PDFe o .sql formatado com o upper case

---
+++ trabalho-bernardo-e-rodrigo.sql
@@ -0,0 +1,179 @@
+CREATE schema SPGS;
+set search_path = SPGS;
+set DATEstyle TO "ISO, DMY";
+
+-- cricação das tabelas
+CREATE TABLE filial (
+	codFilial SERIAL, 
+	cidade VARCHAR(255) NOT NULL, 
+	dtInauguracao DATE NOT NULL, 
+	qtVeiculos INT NOT NULL CHECK(qtVeiculos between 0 and 20),
+	PRIMARY KEY (codFilial)
+);
+
+CREATE TABLE colaborador(
+	cod SERIAL,
+	codFilial  INT NOT NULL,
+	cpf VARCHAR(30) NOT NULL UNIQUE,
+	nome VARCHAR(255) NOT NULL,
+	endereco VARCHAR(255) NOT NULL,
+	salario DOUBLE PRECISION NOT NULL,
+	funcao VARCHAR(255) CHECK(funcao IN('Mecanico', 'Consultor')) NOT NULL,
+	dtAdmissao DATE DEFAULT CURRENT_DATE,
+	PRIMARY KEY (cod),
+	FOREIGN KEY (codFilial) REFERENCES Filial(codFilial)
+);
+
+CREATE TABLE servico(
+	cod SERIAL,
+	descricao VARCHAR(255) NOT NULL,
+	valorMaoObra DOUBLE PRECISION NOT NULL,
+	PRIMARY KEY (cod)
+);
+
+CREATE TABLE colaborador_servico(
+	codColaborador INT NOT NULL,
+	codServico INT NOT NULL,
+	dtInicio DATE NOT NULL,
+	dtFim DATE,
+	ativo BOOLEAN DEFAULT false,
+	PRIMARY KEY (codColaborador, codServico, dtInicio),
+	FOREIGN KEY (codColaborador) REFERENCES colaborador(cod),
+	FOREIGN KEY(codServico) REFERENCES servico(cod)
+);
+	
+CREATE TABLE cliente(
+	cod SERIAL,
+	cpf VARCHAR(30) NOT NULL unique,
+	nome VARCHAR(255) NOT NULL,
+	dtNascimento DATE NOT NULL,
+	pagamentoFinalizado BOOLEAN DEFAULT false,
+	PRIMARY KEY(cod)
+);
+
+CREATE TABLE carro(
+	cod SERIAL,
+	codCliente INT NOT NULL,
+	modelo VARCHAR(255) NOT NULL,
+	tipoCombustivel VARCHAR(255) CHECK(tipoCombustivel IN ('Gasolina','Diesel','Gás','Elétrico')),
+	PRIMARY KEY (cod),
+	FOREIGN KEY (codCliente) REFERENCES cliente(cod)
+);
+
+CREATE TABLE pagamento(
+	cod SERIAL,
+	codNF INT NOT NULL,
+	codCliente INT NOT NULL,
+	valor DOUBLE PRECISION NOT NULL,
+	dtPagamento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(cod, codNF),
+	FOREIGN KEY (codCliente) REFERENCES cliente(cod)
+);
+
+
+CREATE TABLE servico_carro(
+	codCarro INT NOT NULL,
+	codservico INT NOT NULL,
+	dtInicio DATE NOT NULL,
+	dtFim DATE, 
+	status VARCHAR(255) CHECK(status IN('Pendente','Andamento','Finalizado')) DEFAULT 'Pendente',
+	PRIMARY KEY(codCarro, codservico, dtInicio),
+	FOREIGN KEY (codCarro) REFERENCES carro(cod),
+	FOREIGN KEY (codservico) REFERENCES servico(cod)
+);
+
+-- INSERTs nas tabelas
+
+-- tabela filial
+INSERT INTO filial (cidade, dtInauguracao, qtVeiculos) VALUES ('Porto Alegre', '05/01/2010', 10);
+INSERT INTO filial (cidade, dtInauguracao, qtVeiculos) VALUES ('Canoas', '06/05/2019', 5);
+INSERT INTO filial (cidade, dtInauguracao, qtVeiculos) VALUES ('São Leopoldo', '20/06/2020', 3);
+INSERT INTO filial (cidade, dtInauguracao, qtVeiculos) VALUES ('Sapiranga', '05/06/2021', 7);
+INSERT INTO filial (cidade, dtInauguracao, qtVeiculos) VALUES ('Gravataí', '25/02/2018', 13);
+
+-- colaboradores
+INSERT INTO colaborador (codFilial, cpf, nome, endereco, salario, funcao, dtAdmissao) 
+    VALUES (1, '20217852033', 'Bernardo', 'Centro', 1200, 'Mecanico', '10/06/2022');
+INSERT INTO colaborador (codFilial, cpf, nome, endereco, salario, funcao, dtAdmissao) 
+    VALUES (2, '90641865023', 'Nicolas', 'Mathias Velho', 2000, 'Consultor', '20/06/2022');
+INSERT INTO colaborador (codFilial, cpf, nome, endereco, salario, funcao, dtAdmissao) 
+    VALUES (3, '56916504080', 'João', 'Campina', 1300, 'Mecanico', '20/06/2022');
+INSERT INTO colaborador (codFilial, cpf, nome, endereco, salario, funcao) 
+    VALUES (4, '42295174082', 'Bruno', 'Ouro Branco', 1200, 'Mecanico');
+INSERT INTO colaborador (codFilial, cpf, nome, endereco, salario, funcao) 
+    VALUES (5, '20217854653', 'Julio', 'Ideal', 1200, 'Mecanico');
+INSERT INTO colaborador (codFilial, cpf, nome, endereco, salario, funcao, dtAdmissao) 
+    VALUES (3, '51034529013', 'Rodrigo', 'Boa Vista', 2000, 'Consultor', '22/04/2022');
+INSERT INTO colaborador (codFilial, cpf, nome, endereco, salario, funcao, dtAdmissao) 
+    VALUES (2, '17019095020', 'Pedro', 'Centro', 1300, 'Mecanico', '22/04/2022');
+	
+-- servico
+INSERT INTO servico(descricao, valorMaoObra) VALUES ('Troca de óleo', 200);
+INSERT INTO servico(descricao, valorMaoObra) VALUES ('Manutenção de embreagem', 450);
+INSERT INTO servico(descricao, valorMaoObra) VALUES ('Revisão do freio', 150);
+INSERT INTO servico(descricao, valorMaoObra) VALUES ('Alinhamento', 600);
+INSERT INTO servico(descricao, valorMaoObra) VALUES ('Balanceamento', 700);
+INSERT INTO servico(descricao, valorMaoObra) VALUES ('Reparo na lataria', 600);
+INSERT INTO servico(descricao, valorMaoObra) VALUES ('Troca de óleo, checagem do nível de água', 300);
+
+-- colaborador_servico
+INSERT INTO colaborador_servico(codColaborador, codServico, dtInicio) 
+	VALUES (2, 1, '25/07/2021');
+INSERT INTO colaborador_servico(codColaborador, codServico, dtInicio, ativo) 
+	VALUES (1, 2, '20/07/2021', true);
+INSERT INTO colaborador_servico(codColaborador, codServico, dtInicio) 
+	VALUES (3, 5, '10/08/2022');
+INSERT INTO colaborador_servico(codColaborador, codServico, dtInicio, ativo) 
+	VALUES (4, 3, '17/09/2022', true);
+INSERT INTO colaborador_servico(codColaborador, codServico, dtInicio) 
+	VALUES (2, 2, '24/11/2021');
+INSERT INTO colaborador_servico(codColaborador, codServico, dtInicio) 
+	VALUES (1, 1, '30/10/2021');
+INSERT INTO colaborador_servico(codColaborador, codServico, dtInicio) 
+	VALUES (3, 4, '28/02/2022');
+INSERT INTO colaborador_servico(codColaborador, codServico, dtInicio, dtFim) 
+	VALUES (3, 7, '28/02/2022', '10/03/2022');
+INSERT INTO colaborador_servico(codColaborador, codServico, dtInicio, ativo) 
+	VALUES (3, 6, '28/02/2022', true);
+
+-- cliente
+INSERT INTO cliente(cpf, nome, dtNascimento, pagamentoFinalizado)
+	VALUES('07621014075', 'Patrick', '03/05/1999', true);
+INSERT INTO cliente(cpf, nome, dtNascimento)
+	VALUES('96854952034', 'Ivan', '20/05/2002');
+INSERT INTO cliente(cpf, nome, dtNascimento, pagamentoFinalizado)
+	VALUES('69896025037', 'Matheus K.', '15/05/2000', true);
+INSERT INTO cliente(cpf, nome, dtNascimento)
+	VALUES('37307443058', 'Luigi', '24/05/2003');
+INSERT INTO cliente(cpf, nome, dtNascimento, pagamentoFinalizado)
+	VALUES('42850882070', 'Diego', '10/06/2002', true);
+	
+-- carro
+INSERT INTO carro(codCliente, modelo, tipoCombustivel) VALUES (1,'Gol','Gasolina');
+INSERT INTO carro(codCliente, modelo, tipoCombustivel) VALUES (2,'Prius','Elétrico');
+INSERT INTO carro(codCliente, modelo, tipoCombustivel) VALUES (3,'Palio','Gasolina');
+INSERT INTO carro(codCliente, modelo, tipoCombustivel) VALUES (4,'Voyage','Gás');
+INSERT INTO carro(codCliente, modelo, tipoCombustivel) VALUES (5,'Toro','Diesel');
+
+-- pagamento
+INSERT INTO pagamento (codNF, codCliente, valor) VALUES (124141, 1, 600);
+INSERT INTO pagamento (codNF, codCliente, valor) VALUES (124142, 1, 400);
+INSERT INTO pagamento (codNF, codCliente, valor) VALUES (124143, 3, 800);
+INSERT INTO pagamento (codNF, codCliente, valor) VALUES (124144, 5, 600);
+INSERT INTO pagamento (codNF, codCliente, valor) VALUES (124145, 5, 1000);
+
+-- servico_carro
+INSERT INTO servico_carro(codCarro, codservico, dtInicio, dtFim, status)
+	VALUES(1, 2, '20/07/2021', '25/07/2021', 'Finalizado');
+INSERT INTO servico_carro(codCarro, codservico, dtInicio, dtFim, status)
+	VALUES(1, 4, '26/07/2021', '30/07/2021', 'Finalizado');
+INSERT INTO servico_carro(codCarro, codservico, dtInicio, dtFim, status)
+	VALUES(3, 3, '15/09/2022', '05/10/2022', 'Finalizado');
+INSERT INTO servico_carro(codCarro, codservico, dtInicio, dtFim, status)
+	VALUES(5, 6, '14/02/2022', '16/02/2022', 'Finalizado');
+INSERT INTO servico_carro(codCarro, codservico, dtInicio, dtFim, status)
+	VALUES(5, 7, '17/02/2022', '21/03/2022', 'Finalizado');
+INSERT INTO servico_carro(codCarro, codservico, dtInicio)
+	VALUES(2, 1, '27/05/2021');
+INSERT INTO servico_carro(codCarro, codservico, dtInicio)
+	VALUES(4, 2, '28/05/2021');
